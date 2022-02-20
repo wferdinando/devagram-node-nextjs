2832396e254e8571c876412b365b7e869e29efdb
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2022-02-19 21:12:10 -0300

    get do usuario logado e feed pelo id do user

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,8 +1,20 @@
+import { UsuarioModel } from './../../models/UsuarioModel';
+import { RespostaPadraoMsg } from './../../types/RespostaPadraoMsg';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 
-const ususarioEndPoint = (req: NextApiRequest, res: NextApiResponse) => {
-    return res.status(200).json('Usuário autenticado com sucesso!');
+const ususarioEndPoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+    try {
+        const { userId } = req?.query; //pega o id do usuário que está no token decodificado no login.
+        //como buscar os dados do usuário no banco?
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null;
+        return res.status(200).json(usuario);
+    } catch (e) {
+        console.log(e);
+    }
+    return res.status(400).json({ erro: 'Não foi possível obter os dados do usuário!' });
 }
 
-export default validarTokenJWT(ususarioEndPoint);
+export default validarTokenJWT(conectarMongoDB(ususarioEndPoint));
