e40c05dd34969fffa0d7c1526b702a7910c01b1a
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2022-02-21 21:15:00 -0300

    Correcao Update Usuário

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -19,7 +19,7 @@ const handler = nc()
             }
 
             const { nome } = req?.body;
-            if (nome && nome.length < 2) {
+            if (nome && nome.length > 2) {
                 usuario.nome = nome;
             }
             const { file } = req;
